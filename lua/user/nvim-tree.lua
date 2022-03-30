92adac8667a1945ad246d9724a1aa9e20d7835bb
Cyprian Kleist <ckleist@task.gda.pl>
2022-03-31 00:35:02 +0200

    remove some annoying mappings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -34,6 +34,7 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  auto_reload_on_write = true,
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
@@ -86,8 +87,8 @@ nvim_tree.setup {
     mappings = {
       custom_only = false,
       list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
+        -- { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        -- { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
       },
     },
