5c46bb2d62a227c8a5c9a32c191d68263e5b5a82
Cyprian Kleist <cyp2505@wp.pl>
2022-05-10 09:48:04 +0200

    [issue-]-adjust nvim tree to new api

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -21,6 +21,18 @@ vim.g.nvim_tree_icons = {
   },
 }
 
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_root_folder_modifier = ":t"
+vim.g.nvim_tree_quit_on_open = 0
+vim.g.nvim_tree_disable_window_picker = 0
+vim.g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 1,
+    tree_width = 30,
+  }
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -71,7 +83,7 @@ nvim_tree.setup {
   },
   filters = {
     dotfiles = false,
-    custom = {},
+    custom = {}
   },
   git = {
     enable = true,
@@ -99,15 +111,15 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
+  -- quit_on_open = 0,
+  -- git_hl = 1,
+  -- disable_window_picker = 0,
+  -- root_folder_modifier = ":t",
+  -- show_icons = {
+  --   git = 1,
+  --   folders = 1,
+  --   files = 1,
+  --   folder_arrows = 1,
+  --   tree_width = 30,
+  -- },
 }
