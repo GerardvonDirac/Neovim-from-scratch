6648184567fe84f92d9e84cf9bdaac2846fae236
Cyprian Kleist <ckleist@task.gda.pl>
2022-07-26 13:52:36 +0200

    [issue-]-fix lsp after update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -59,7 +59,7 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
@@ -90,12 +90,16 @@ nvim_tree.setup {
     ignore = true,
     timeout = 500,
   },
+  actions = {
+    open_file = {
+      resize_window = true,
+    },
+  },
   view = {
     width = 30,
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
