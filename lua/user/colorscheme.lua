c623180a25908fca0ad19219e76602890642e571
Cyprian Kleist <ckleist@task.gda.pl>
2023-07-04 08:19:29 +0200

    [issue-]-feat: better color in diff mode

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,3 +1,16 @@
+local dracula = require("dracula")
+local colors = require('dracula').colors()
+dracula.setup({
+  -- show the '~' characters after the end of buffers
+  show_end_of_buffer = true, -- default false
+  -- set italic comment
+  italic_comment = true, -- default false
+  -- overrides the default highlights with table see `:h synIDattr`
+  overrides = {
+    DiffText = { fg = colors.bg, bg = colors.orange },
+  },
+})
+
 vim.cmd [[
 try
   colorscheme dracula
