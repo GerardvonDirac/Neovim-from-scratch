aa067ad761af82940e38619a6b1c7bbf4f089ec8
Cyprian Kleist <ckleist@task.gda.pl>
2023-10-09 09:37:43 +0200

    [issue-]-feat: add highlght-whitespace plugin

---
+++ lua/user/highlight-whitespace.lua
@@ -0,0 +1,29 @@
+local status_ok, highlight_whitespace = pcall(require, "highlight-whitespace")
+if not status_ok then
+  return
+end
+
+highlight_whitespace.setup {
+  tws = '\\s\\+$',
+  clear_on_winleave = false,
+  user_palette = {
+    markdown = {
+      tws = 'RosyBrown',
+      ['\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@='] = 'CadetBlue3',
+      ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'SkyBlue1',
+      ['\\t\\+'] = 'plum4',
+    },
+    csv = {
+      tws = 'bg',
+      ['\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@='] = 'bg',
+      ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'bg',
+      ['\\t\\+'] = 'plum4',
+    },
+    other = {
+      tws = 'PaleVioletRed',
+      ['\\(\\S\\)\\@<=\\s\\(,\\)\\@='] = 'coral1',
+      ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'LightGoldenrod3',
+      ['\\t\\+'] = 'plum4',
+    }
+  }
+}
